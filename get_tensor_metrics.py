63274d9fc786295a0a62370dc8d3507e085e181e
francl <francl@mit.edu>
2021-03-01 11:56:16 -0500

    Removing outdated versions of files in repo

--- get_tensor_metrics.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-def record_tensor_mean(tensor_in,update_collection="metrics_update",
-                        metrics_collection="metrics_out"):
-    mean_value,mean_update = tf.metrics.mean_tensor(tensor_in,
-                                                    metrics_collections=metrics_collection,
-                                                    updates_collections=update_collection)
-    return mean_value,mean_update
--- view_arch.py
+++
@@ -1,4 +0,0 @@
-#!/cm/shared/openmind/anaconda/2.5.0/bin/python
-import numpy as np
-import sys
-print(np.load(sys.stdin.read().rstrip()))
